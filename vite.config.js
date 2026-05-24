51ff271dcf51f4caa6a5f7a0d8249a5dcffb2ee0
kelly <kellychen881110@gmail.com>
2026-05-24 11:41:54 +0800

    error fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/K_recipeweb/',
+  base: '/K_recipe_web/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
