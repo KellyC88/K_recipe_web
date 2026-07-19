1baffb40e1f9171aa36a13905673cda27a258f42
kelly <kellychen881110@gmail.com>
2026-07-19 11:48:11 +0800

    add existing code

--- src/data/ingredients/3.js
+++ src/data/ingredients/3.js
@@ -1,10 +1,13 @@
-export default [
-  'butter',
-  'flour',
-  'sugar',
-  'eggs',
-  'milk',
-  'salt',
-  'yeast',
-  'water',
-];
+export default {
+  servings: 1,
+  ingredients: [
+    { name: 'butter', quantity: 1, unit: 'slice' },
+    { name: 'flour', quantity: 50, unit: 'g' },
+    { name: 'sugar', quantity: 25, unit: 'g' },
+    { name: 'eggs', quantity: 1, },
+    { name: 'milk', quantity: 100, unit: 'ml' },
+    { name: 'salt', quantity: 5, unit: 'g' },
+    { name: 'yeast', quantity: 10, unit: 'g' },
+    { name: 'water', quantity: 100, unit: 'ml' },
+  ],
+};
--- src/data/ingredients/4.js
+++ src/data/ingredients/4.js
@@ -1,10 +1,11 @@
-export default [
-  'tea leaves',
-  'water',
-  'lemon',
-  'honey',
-  'ginger',
-  'mint',
-  'cinnamon',
-  
-];
+export default {
+  servings: 1,
+  ingredients: [
+    { name: 'mint leaves', quantity: 2, unit: 'slice' },
+    { name: 'water', quantity: 250, unit: 'mL' },
+    { name: 'lemon juice', quantity: 15, unit: 'mL' },
+    { name: 'honey', quantity: 10, unit: 'g' },
+    { name: 'ginger', quantity: 5, unit: 'g' },
+    { name: 'cinnamon', quantity: 3, unit: 'g' },
+  ],
+};
