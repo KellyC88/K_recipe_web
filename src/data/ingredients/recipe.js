f54689ce40945fe754bbe1feda55e9e036ffd13d
kelly <kellychen881110@gmail.com>
2026-07-25 12:29:38 +0800

    add existing code

--- src/data/ingredients/recipe.js
+++ src/data/ingredients/recipe.js
@@ -2,6 +2,20 @@ export default [
     {id: 1, recipe: [
         { step: 1, description: 'Combine all ingredients in a blender and blend until smooth.' },
         { step: 2, description: 'Pour into a glass and serve immediately.' },
-    ]}
+    ]},
+    {id: 2, recipe: [
+        { step: 1, description: 'Toast the sourdough bread until lightly crispy.' },
+        { step: 2, description: 'Top with yogurt egg salad.' },
+        {step: 3, description: 'Add sliced avocado and fresh fruits.' },
+    ]},
+    {id: 3, recipe: [
+        { step: 1, description: 'Whisk butter, flour, sugar, eggs, milk, salt, yeast, and water into a smooth dough.' },
+        { step: 2, description: 'Bake until golden and fluffy.' },
+        { step: 3, description: 'Top the croissant with poedered sugar and almonds.' },
+    ]},
+    {id: 4, recipe: [
+        { step: 1, description: 'Blend mint, water, lemon juice, honey, ginger, and cinnamon until smooth.' },
+        { step: 2, description: 'Pour into a glass and serve chilled.' },
+    ]},
 ]
 import Recipe from './recipe.js'
